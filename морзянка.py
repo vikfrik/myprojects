4f57abef2caae044016c51c8b728c5ef4968dfdf
vikfrik <114554675+vikfrik@users.noreply.github.com>
2022-09-28 12:16:38 +0700

    Add files via upload

---
+++ морзянка.py
@@ -0,0 +1,17 @@
+a = str('A B W G D E V Z I J K L M N O P R S T U F H C Q X')
+abc = list(a)
+print (a)
+m = str('.- -... .-- --. -.. . ...- --.. .. .--- -.- .-.. -- - --- .--. .-. ... - ..- ..-. .... -.-. ---.  - ..- ..-. .... -.-. --.- -..-')
+morze = list(m)
+print morze.split(''))
+text = input()
+ind_m = ''
+for i in range (len(text_list)):
+    ind = abc.index(text[i])
+    ind_m = ind_m + norze[ind]
+print (indm)
+    
+    
+    
+    
+    
